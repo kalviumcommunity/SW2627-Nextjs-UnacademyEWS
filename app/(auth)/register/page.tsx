af22db84e905cc0e0e8e87273c5e165c1841c79a
Harshit Pushkarna <harshitpushkarnais@gmail.com>
2026-08-27 11:23:00 +0530

    test: remove CI test error

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -54,9 +54,6 @@ export default function RegisterPage() {
         }
     }, [state]);
 
-    const testError: string = 123;
-
-
     useEffect(() => {
         if (state?.errors) {
             // eslint-disable-next-line react-hooks/set-state-in-effect
